68d318777aab89f34691da8193de26368ae02c5b
Franco Gimenez <gimenezfrancotomas96@gmail.com>
2023-01-26 23:50:35 -0300

    tercer commit probando cosas nuevas en js

---
+++ javascript/javaindex.js
@@ -0,0 +1,12 @@
+document.getElementById('imagenFondo').addEventListener('click', function() {
+    document.body.style.backgroundImage = 'url(imagenes/fondo5.webp)';
+    document.body.style.color = 'blue'
+}
+)
+
+document.getElementById('back2back').addEventListener('click', function() {
+    document.body.style.backgroundImage = 'url(imagenes/fondo2.webp)';
+    document.body.style.color = 'darkmagenta'
+}
+)
+
